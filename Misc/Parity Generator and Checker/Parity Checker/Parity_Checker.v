8669e8f0d9a3b6cdadb747743de8d02cdec387bf
Devesh_Bhagwani <126229184+DeveshBhagwani@users.noreply.github.com>
2026-05-19 22:56:33 +0530

    Create Parity_Checker.v

---
+++ Misc/Parity Generator and Checker/Parity Checker/Parity_Checker.v
@@ -0,0 +1,8 @@
+module Parity_Checker_Even(
+  input [4:0] received,
+  output checker_even
+);
+  assign checker_even = (received[0] == ^received[4:1]) ? 1'b0 : 1'b1;
+  // 0 => transmitted corrctly
+  // 1 => error in transmission
+endmodule
